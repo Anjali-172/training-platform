ad1812ed949ec10009e4922694d72193de22bc28
Anjali Anandi <anjalianandi.152@gmail.com>
2026-02-20 15:04:07 +0530

    Initial commit - Training Platform

---
+++ script.js
@@ -0,0 +1,22 @@
+// Future enhancements can go here
+function toggleMenu(){
+    document.querySelector(".nav-menu").classList.toggle("active");
+}
+// Fade-in animation on load
+window.addEventListener("load", function(){
+    document.querySelector(".hero-content").classList.add("active");
+});
+// Button click function
+function explorePrograms(){
+    alert("Explore Programs section coming soon 🚀");
+}
+function explorePrograms(){
+    document.getElementById("programs").scrollIntoView({ 
+        behavior: "smooth" 
+    });
+}
+function toggleDropdown(element) {
+    const menu = element.nextElementSibling;
+    menu.style.display =
+        menu.style.display === "flex" ? "none" : "flex";
+}
